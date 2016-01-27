d6d5b8a50d379ca05cbe85530e709c3df1e5c519
h2so5 <mail@h2so5.net>
2016-01-27 21:00:59 +0900

    fix mocha tests

--- test/test.js
+++ test/test.js
@@ -8,7 +8,7 @@ describe('Repository', function() {
       const repo = geit('https://example.com/');
       repo.refs(function(refs, err) {
         assert.deepEqual(refs, null);
-        assert.deepEqual(err, new Error('Not Found'));
+        assert.deepEqual(err.message, '404 Not Found');
         done();
       });
     });
@@ -20,7 +20,7 @@ describe('Repository', function() {
       const repo = geit('https://github.com/h2so5/geit.git');
       repo.tree('null', function(tree, err) {
         assert.deepEqual(tree, null);
-        assert.deepEqual(err, new Error('no such branch or tag'));
+        assert.deepEqual(err.message, 'no such branch or tag');
         done();
       });
     });
@@ -32,7 +32,17 @@ describe('Repository', function() {
       const repo = geit('https://github.com/h2so5/geit.git');
       repo.blob('b8d6926f1b80437d8ddc8a75e21de886132ff838', function(blob, err) {
         assert.deepEqual(blob, null);
-        assert.deepEqual(err, new Error('blob object not found'));
+        assert.deepEqual(err.message, 'blob object not found');
+        done();
+      });
+    });
+
+    it('should return an error when the id does not exist', function(done) {
+      this.timeout(10000);
+      const repo = geit('https://github.com/h2so5/geit.git');
+      repo.blob('b8d6926f1b80437d8ddc8a75e21de886132ff839', function(blob, err) {
+        assert.deepEqual(blob, null);
+        assert.deepEqual(err.message, 'object not found');
         done();
       });
     });
